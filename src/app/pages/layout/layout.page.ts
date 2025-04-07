9c238966bd6e91895398cd764a3bc5cd0853d930
leo-morales-dev <leonardo.morales.work@gmail.com>
2025-04-07 16:56:18 -0600

    ruta2

--- src/app/pages/layout/layout.page.ts
+++ src/app/pages/layout/layout.page.ts
@@ -5,7 +5,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarComponent],
-  template: `<app-sidebar />`
+  imports: [SidebarComponent, RouterOutlet], // 👈 se agrega RouterOutlet aquí
+  template: `
+    <div class="d-flex">
+      <app-sidebar />
+      <div class="flex-grow-1 p-4" style="min-height: 100vh;">
+        <router-outlet></router-outlet> <!-- 👈 aquí se renderiza el contenido -->
+      </div>
+    </div>
+  `
 })
 export class LayoutPage {}
